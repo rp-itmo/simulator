fb0a736f293bb382dd3fc11957b63144cf795b9e
Infinity2Beyond <ashpokemui123@gmail.com>
2026-05-14 09:24:52 +0300

    First Setup
    Setup Pygame in Enviroment, created sample pygame_render file

--- src/simulator/main.py
+++ src/simulator/main.py
@@ -3,6 +3,8 @@ import numpy as np
 
 from physics import PhysicsEngine
 from renderer import Renderer
+from pygame_renderer import PygameRenderer
+
 from world import World
 
 from objects import RobotTree, TwoLink, Tree7, CartPole
@@ -15,6 +17,7 @@ def main():
 
     physics = PhysicsEngine(fd_solver, gravity=[0.0, -9.81, 0.0])
     renderer = Renderer()
+    # renderer = PygameRenderer()
 
     world = World(physics, renderer)
 
---
+++ src/simulator/pygame_renderer.py
@@ -0,0 +1,7 @@
+import pygame
+import numpy as np
+import sys
+
+class PygameRenderer:
+    def __init__(self):
+        pygame.init()
